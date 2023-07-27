d59d785774af623debee6da1a7635f917b6ad119
jfrlysona <jfrlysona@icloud.com>
2023-07-28 01:51:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+//First task 
+
+// let myName = prompt('What is your name?');
+// console.log(`Welcome ${myName}!`)
+
+
+
+//second task
+
+// let number1 = prompt('Enter first number:');
+// let number2 = prompt('Enter second number:');
+
+// if (number1 > number2) {
+//     console.log('The biggest number: ' + number1);
+// } else if (number2 > number1) {
+//     console.log('The biggest number: ' + number2);
+// } else {
+//     console.log('Numbers are equal');
+// }
+
+
+
+//third task
+
+// let a=prompt('Enter the number:'); 
+
+// if(a>0) {
+//     console.log(`The number ${a} is positive`)
+// } else if (a<0) {
+//     console.log(`The number ${a} is negative`)
+// } else if (a==0) {
+//     console.log(`The number is 0`)
+// }
